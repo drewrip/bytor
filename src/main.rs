a3ee177b02554c2ea66a5dd91c08c9e35c469c81
Drew Ripberger <drew.ripberger@gmail.com>
2023-12-26 15:02:11 -0500

    lalrpop file structure setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use lalrpop_util::lalrpop_mod;
+
+
+pub mod ast;
+lalrpop_mod!(pub rascal_grammar); // synthesized by LALRPOP
+
+#[test]
+fn calculator1() {
+    assert!(rascal_grammar::TermParser::new().parse("22 + 22").is_ok());
+    assert!(rascal_grammar::TermParser::new().parse("22 +").is_err());
+    assert!(rascal_grammar::TermParser::new().parse("(22 + 12) * 2").is_err());
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello from the Rascal compiler");
 }
