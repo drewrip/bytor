cda11c81faf98be6fdda614842a9f432541b2231
Drew Ripberger <drew.ripberger@gmail.com>
2023-12-28 02:14:50 -0500

    statements in the grammar and basic cli

--- src/ast.rs
+++ src/ast.rs
@@ -1,8 +1,45 @@
 #[derive(Debug)]
 pub enum Expr {
-    Term(i32),
+    Term(Box<Term>),
     Add(Box<Expr>, Box<Expr>),
     Sub(Box<Expr>, Box<Expr>),
     Mult(Box<Expr>, Box<Expr>),
     Div(Box<Expr>, Box<Expr>),
+}
+
+#[derive(Debug)]
+pub enum Term {
+    Num(i32),
+    Expr(Box<Expr>),
+}
+
+#[derive(Debug)]
+pub enum AssignOp {
+    Assign,
+    AddAssign,
+    SubAssign,
+    MultAssign,
+    DivAssign,
+}
+
+#[derive(Debug)]
+pub enum Stmt {
+    Assign(Box<Var>, AssignOp, Box<Expr>),
+}
+
+#[derive(Debug)]
+pub struct Var {
+    pub type_t: Type,
+    pub ident: String,
+}
+
+#[derive(Debug)]
+pub enum Type {
+    Int64,
+    Int32,
+    UInt64,
+    UInt32,
+    Float64,
+    Float32,
+    Unknown,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,38 @@
+use std::fs;
+
 use lalrpop_util::lalrpop_mod;
+use clap::Parser;
 
 pub mod ast;
+
+/// Compiler for the Rascal language
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// Input Rascal source file
+    infile: String,
+
+    /// Name of output binary
+    #[arg(short, long, default_value = "a.out")]
+    outfile: String,
+}
 
 lalrpop_mod!(pub rascal_grammar);
 
 fn main() {
-    let source 
-    println!("Hello from the Rascal compiler");
+    let args = Args::parse();
+    
+    let src_file = fs::read_to_string(args.infile).expect("ERROR: couldn't find source file");
+    let root = rascal_grammar::ProgramParser::new().parse(&src_file);
+    println!("{:?}", root);
 }
 
 #[test]
 fn root_parser_passing1() {
     let source = r#"
     program
-        22 + 2
+        let x = 9;
+        let y = 10;
     end
     "#;
     assert!(rascal_grammar::ProgramParser::new().parse(source).is_ok());
@@ -23,7 +42,7 @@ fn root_parser_passing1() {
 fn root_parser_failing1() {
     let source = r#"
     program
-        22 +
+        1 + 2;
     end
     "#;
     assert!(rascal_grammar::ProgramParser::new().parse(source).is_err());
