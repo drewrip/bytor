747db3ebefcb0e8adcba4803ac7b847965218536
Drew Ripberger <drew.ripberger@gmail.com>
2023-12-27 22:49:10 -0500

    clear test case

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,30 @@
 use lalrpop_util::lalrpop_mod;
 
-
 pub mod ast;
-lalrpop_mod!(pub rascal_grammar); // synthesized by LALRPOP
 
-#[test]
-fn calculator1() {
-    assert!(rascal_grammar::TermParser::new().parse("22 + 22").is_ok());
-    assert!(rascal_grammar::TermParser::new().parse("22 +").is_err());
-    assert!(rascal_grammar::TermParser::new().parse("(22 + 12) * 2").is_err());
-}
+lalrpop_mod!(pub rascal_grammar);
 
 fn main() {
+    let source 
     println!("Hello from the Rascal compiler");
+}
+
+#[test]
+fn root_parser_passing1() {
+    let source = r#"
+    program
+        22 + 2
+    end
+    "#;
+    assert!(rascal_grammar::ProgramParser::new().parse(source).is_ok());
+}
+
+#[test]
+fn root_parser_failing1() {
+    let source = r#"
+    program
+        22 +
+    end
+    "#;
+    assert!(rascal_grammar::ProgramParser::new().parse(source).is_err());
 }
