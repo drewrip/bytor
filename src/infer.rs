70e6620314a54e7c52ea980ac2e44ad64387723a
Drew Ripberger <drew.ripberger@gmail.com>
2024-06-19 18:16:59 -0400

    start to tests for infer

--- src/infer.rs
+++ src/infer.rs
@@ -4,14 +4,30 @@ use thiserror::Error;
 
 #[derive(Error, Debug)]
 pub enum TypeError {
-    #[error("The resulting WASM failed validation: {0}")]
-    ValidateFailed(String),
-    #[error("Couldn't write output binary: {0}")]
-    BinaryWrite(String),
-    #[error("Couldn't compile binary: {0}")]
-    CompilationFailed(String),
+    #[error("No definition for variable: {0}")]
+    IdentNotFound(String),
 }
 
 pub fn infer(gamma: SymbolStack) -> Result<(), TypeError> {
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::ast::Node;
+    use crate::semantic::SymbolTable;
+    use crate::symbol::{new_symbol, new_var};
+    use crate::types::Type;
+
+    #[test]
+    fn simple_passing() {
+        let ctx: SymbolStack = vec![SymbolTable {
+            table: vec![(new_symbol("x".into()), new_var(Type::Int32, Node::Null))]
+                .into_iter()
+                .collect(),
+        }];
+        let res = infer(ctx);
+        assert!(res.is_ok());
+    }
 }
